7a3aa5d3983548017e1c894bbf2176cb776cc90d
asadhossain <developerasad007@gmail.com>
2023-03-02 14:44:47 +0600

    a

---
+++ contact.php
@@ -0,0 +1,4 @@
+<h3>User Name: <?php echo $userName;    ?></h3>
+<h3>Email: </h3>
+<h3>Password:</h3>
+
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+
+<?php
+if (isset($_POST['btn']))
+$userName = $_POST ['userName'];
+$email = $_POST ['email'];
+$password = $_POST ['password'];
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +14,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>     "<h3>hello workd</h3>"
+<body>    
+
+<h3>User Name: <?php echo $userName;    ?></h3>
+<h3>Email: </h3>
+<h3>Password:</h3>
+
+
+    <form action="contact.php" method= "POST">
+
+    <label for=""> User Name</label> <br>
+    <input type="text" name= "userName"><br>
+    <label for=""> Email</label><br>
+    <input type="email" name = "email"><br>
+    <label for=""> Password</label><br>
+    <input type="Password" name= "Password"><br><br>
+    <input type ="submit" name = "btn" value = "Submit Data">
+
+    </form>
+
+
 
     <?php 
-    
-    
-    
-    
     
     
     ?>
@@ -22,3 +45,4 @@
 
 </body>
 </html>
+
